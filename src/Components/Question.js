8e7d02b4e5079f79d7799eb61e711ceaced1822d
Dmitry <dkumachev18@gmail.com>
2021-12-28 13:05:31 +0400

    some-logic

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -3,13 +3,15 @@ import Answer from "./Answer";
 const Question = () => {
     return (
         <div>
-        <div>Question</div>
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
+            <div className="question">Text of the question</div>
+            <div className="answers">
+                <Answer />
+                <Answer />
+                <Answer />
+                <Answer />
+            </div>
         </div> 
-        )
-}
+    );
+};
 
 export default Question;
